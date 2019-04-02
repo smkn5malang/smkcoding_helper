168470e4311b6d2d7b0e0a09e910234760eb6b8a
Welly <wellyimanuel86@gmail.com>
2019-04-02 18:50:24 +0700

    login

---
+++ app/src/main/java/com/wellyimmanuel/helper/Login.kt
@@ -0,0 +1,35 @@
+package com.wellyimmanuel.helper
+
+import android.app.ProgressDialog
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        btnLogin.setOnClickListener {
+            val email = inputEmail.text.toString()
+            val password = inputPassword.text.toString()
+            if (email.isEmpty()|| password.isEmpty()) {
+                Toast.makeText(this, "Please Insert Email and Password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if(email == "admin01@gmail.com" || password == "admin01"){
+                val progressDialog = ProgressDialog(this,
+                    R.style.Theme_MaterialComponents_Light_Dialog)
+                progressDialog.isIndeterminate = true
+                progressDialog.setMessage("Loading...")
+                progressDialog.show()
+                val intent = Intent (this,Home::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+}
--- app/src/main/java/com/wellyimmanuel/helper/MainActivity.kt
+++ app/src/main/java/com/wellyimmanuel/helper/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnLogin.setOnClickListener{
-            val intent = Intent (this, LoginActivity::class.java)
+            val intent = Intent (this, Login::class.java)
             startActivity(intent)
         }
 
