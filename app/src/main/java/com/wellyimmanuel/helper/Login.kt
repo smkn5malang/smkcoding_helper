49c6adf080f18b8ecbd4905c58308f4db02f4098
Welly <wellyimanuel86@gmail.com>
2019-04-04 15:13:26 +0700

    autent

--- app/src/main/java/com/wellyimmanuel/helper/Login.kt
+++ app/src/main/java/com/wellyimmanuel/helper/Login.kt
@@ -4,7 +4,9 @@ import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
@@ -20,16 +22,24 @@ class Login : AppCompatActivity() {
                 Toast.makeText(this, "Please Insert Email and Password", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            if(email == "admin01@gmail.com" || password == "admin01"){
-                val progressDialog = ProgressDialog(this,
-                    R.style.Theme_MaterialComponents_Light_Dialog)
-                progressDialog.isIndeterminate = true
-                progressDialog.setMessage("Loading...")
-                progressDialog.show()
-                val intent = Intent (this,Home::class.java)
-                startActivity(intent)
-                finish()
-            }
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener{
+
+                    if (!it.isSuccessful){ return@addOnCompleteListener
+                        val intent = Intent (this, MainActivity::class.java)
+                        startActivity(intent)
+                    }
+                    else
+                        Toast.makeText(this, "Succesfully Login", Toast.LENGTH_SHORT).show()
+                    val intent = Intent (this, Home::class.java)
+                    startActivity(intent)
+                }
+                .addOnFailureListener{
+                    Log.d("Main", "Failed Login: ${it.message}")
+                    Toast.makeText(this, "Email/Password incorrect", Toast.LENGTH_SHORT).show()
+
+
+                }
         }
     }
 }
